c09636c0d9428100b03f461b65edd08d13999dcc
Roman Svintitskyy <70953467+romansvintitskyy@users.noreply.github.com>
2022-11-27 17:18:12 -0600

    Add files via upload
    connected frontend and backend

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import speech_recognition as sr
+from quote_generator import quote_pop
+import ssl
+
+# This is a terrible practice data-privacy-wise but we needed a quick fix
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
 
 def testing():
     print("Python works!!")
@@ -10,8 +17,11 @@ def speech_to_text():
         print("Say Something")
         audio = r.listen(source)
         voice_data = r.recognize_google(audio)
+       # quote = quote_pop(voice_data)
         print(voice_data)
-        return voice_data
+        #print(quote)
+        return voice_data #, quote
 
 if __name__ == "__main__":
-    speech_to_text()
+    text = speech_to_text()
+    quote_pop(text)
